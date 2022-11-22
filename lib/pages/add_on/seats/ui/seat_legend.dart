f4ad210eccc2141d32bcd083ce1f7f1a037273a7
Zaldy Pagaduan <zaldypagaduanjr@gmail.com>
2022-11-22 17:07:05 +0800

    updated wrap alignment

--- lib/pages/add_on/seats/ui/seat_legend.dart
+++ lib/pages/add_on/seats/ui/seat_legend.dart
@@ -21,59 +21,55 @@ class SeatsLegend extends StatelessWidget {
       children: [
         kVerticalSpacerMini,
         const Text("Seating options", style: kMediumMedium),
-        SizedBox(
-          height: 140,
-          child: Wrap(
-            spacing: 8,
-            runSpacing: 10,
-            direction: Axis.vertical,
-            children: [
-              ...(availableType ?? []).map(
-                (e) {
-                  return SizedBox(
-                    width: 170,
-                    height: 40,
-                    child: Row(
-                      children: [
-                        SizedBox(
-                          height: 20,
-                          width: 20,
-                          child: DecoratedBox(
-                            decoration: BoxDecoration(
-                              color: (mapColor ?? {})[e.serviceID],
-                              borderRadius: BorderRadius.circular(4),
-                            ),
+        Wrap(
+          spacing: 20,
+          runSpacing: 10,
+          children: [
+            ...(availableType ?? []).map(
+              (e) {
+                return SizedBox(
+                  width: 150,
+                  height: 40,
+                  child: Row(
+                    children: [
+                      SizedBox(
+                        height: 20,
+                        width: 20,
+                        child: DecoratedBox(
+                          decoration: BoxDecoration(
+                            color: (mapColor ?? {})[e.serviceID],
+                            borderRadius: BorderRadius.circular(4),
                           ),
                         ),
-                        kHorizontalSpacerMini,
-                        Flexible(child: Text(e.description ?? ""))
-                      ],
-                    ),
-                  );
-                },
-              ).toList(),
-              SizedBox(
-                width: 170,
-                height: 40,
-                child: Row(
-                  children: [
-                    SizedBox(
-                      height: 20,
-                      width: 20,
-                      child: DecoratedBox(
-                        decoration: BoxDecoration(
-                          color: Colors.grey,
-                          borderRadius: BorderRadius.circular(4),
-                        ),
+                      ),
+                      kHorizontalSpacerMini,
+                      Flexible(child: Text(e.description ?? ""))
+                    ],
+                  ),
+                );
+              },
+            ).toList(),
+            SizedBox(
+              width: 150,
+              height: 40,
+              child: Row(
+                children: [
+                  SizedBox(
+                    height: 20,
+                    width: 20,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.circular(4),
                       ),
                     ),
-                    kHorizontalSpacerMini,
-                    const Flexible(child: Text("Unavailable"))
-                  ],
-                ),
-              )
-            ],
-          ),
+                  ),
+                  kHorizontalSpacerMini,
+                  const Flexible(child: Text("Unavailable"))
+                ],
+              ),
+            )
+          ],
         ),
       ],
     );
