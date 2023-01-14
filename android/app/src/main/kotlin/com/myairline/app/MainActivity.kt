021cb89598de74cd7546610c903c96f4c6e9fb26
Darius <dariuspepo@gmail.com>
2023-01-14 20:43:30 +0700

    added insider

--- android/app/src/main/kotlin/com/myairline/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/myairline/app/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.myairline.mobileapp
 
-import androidx.annotation.NonNull
-import com.useinsider.insider.flutter_insider.FlutterInsiderPlugin
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
 
-class MainActivity: FlutterActivity() {}
+class MainActivity: FlutterActivity() {
+}
