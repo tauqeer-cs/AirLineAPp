46cdd8fd5a2e313cf491c1af3ff4f247e63907b7
Tauqeer <tahmed@covalience.com>
2023-02-14 15:31:59 +0700

    User insider iOS settings

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,32 @@
 import UIKit
 import Flutter
 
+
+
+//class AppDelegate: UIResponder, UIApplicationDelegate,
+
+
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate  {
+    
+    let appGroup = "group.com.myairline.mobileapp.uat"
+
+    
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
+    
+      UNUserNotificationCenter.current().delegate = self
+
+      //insider api key 9NSEqzLBz0quco87ih2AwhG0IUV7suyj
+      
+      Insider.initWithLaunchOptions(launchOptions, partnerName: "myairlineuat", appGroup: appGroup)
+      Insider.register(withQuietPermission: false)
+      
+      
+      GeneratedPluginRegistrant.register(with: self)
+      
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
