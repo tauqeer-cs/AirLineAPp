622359d0e15c6090dc8117db24dd43359cc66ad0
Tauqeer <tahmed@covalience.com>
2023-03-16 17:28:39 +0700

    Version up

---
+++ lib/pages/check_in/ui/checkin_listing.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CheckingListing extends StatelessWidget {
+  const CheckingListing({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
