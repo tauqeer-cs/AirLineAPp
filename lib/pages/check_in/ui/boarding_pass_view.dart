c3e917ceb103e7c866e81084c6535817dbd3b42b
Grappetite-tauqeer <41113679+Grappetite-tauqeer@users.noreply.github.com>
2023-05-30 09:28:58 +0700

    Spelling fix

--- lib/pages/check_in/ui/boarding_pass_view.dart
+++ lib/pages/check_in/ui/boarding_pass_view.dart
@@ -3,14 +3,10 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:form_builder_validators/form_builder_validators.dart';
-
 import '../../../data/responses/boardingpass_passenger_response.dart';
 import '../../../theme/spacer.dart';
 import '../../../theme/styles.dart';
 import '../../../theme/typography.dart';
-import '../../../widgets/forms/app_input_text.dart';
-import '../../auth/pages/signup/ui/personal_detail/address_input.dart';
 import '../../select_change_flight/ui/booking_refrence_label.dart';
 import '../bloc/check_in_cubit.dart';
 import 'check_in_steps.dart';
